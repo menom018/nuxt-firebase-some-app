2333c31ce5809d84a6cfc2f86183c0c579c2d096
meno.m <mokuya18@gmail.com>
2019-08-17 14:41:44 +0900

    [add] nuxt-createで再作成

---
+++ nuxt-firebase-some-app/.eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true
+  },
+  parserOptions: {
+    parser: '@typescript-eslint/parser'
+  },
+  extends: [
+    '@nuxtjs',
+    'prettier',
+    'prettier/vue',
+    'plugin:prettier/recommended',
+    'plugin:nuxt/recommended'
+  ],
+  plugins: [
+    'prettier',
+    '@typescript-eslint'
+  ],
+  // add your custom rules here
+  rules: {
+    'nuxt/no-cjs-in-config': 'off',
+    '@typescript-eslint/no-unused-vars': 'error'
+  }
+}
